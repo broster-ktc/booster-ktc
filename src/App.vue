ff57cfd5bb7dcc9cecaa9881b08a03c4a1628376
yusukezuck <yilcp76211@yahoo.co.jp>
2025-01-27 18:13:00 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div></div>
+  </header>
+
+  <main>
+    <p>aaa</p>
+  </main>
+</template>
+
+<style scoped></style>
